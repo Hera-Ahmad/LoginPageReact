6158b14bf95d090a7dbc6583f6d03ffe6da069d8
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-17 20:45:18 +0530

    add

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,14 @@
-import { MdMessage } from "react-icons/md";
-
- const Button = () => {
+import styles from "./Button.module.css";
+const Button = ({ isOutline, icon, text, ...rest }) => {
   return (
-    <div><MdMessage />Button</div>
-  )
-}
+    <button
+      {...rest}
+      className={isOutline ? styles.outline_btn : styles.primary_btn}
+    >
+      {icon} 
+      {text}
+    </button>
+  );
+};
 
 export default Button;
--- src/components/contactform/ContactForm.jsx
+++ src/components/contactform/ContactForm.jsx
@@ -1,13 +1,54 @@
-import styles  from "./Contact.module.css";
-
+import styles from "./Contact.module.css";
+import Button from "../Button/Button";
+import { MdMessage } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 
 const ContactForm = () => {
-  return (<section className={styles.container}>
-<div className={styles.contact_form}>
-    < Button />
-</div>
-<div className={styles.contact_Image}></div></section>
-);
+  return (
+    <section className={styles.container}>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+        </div>
+
+        <Button
+          isOutline={true}
+          text="VIA EMAIL FORM"
+          icon={<HiMail fontSize="24px" />}
+        />
+
+        <form>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+
+          <div className={styles.form_control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" />
+          </div>
+
+          <div className={styles.form_control}>
+            <label htmlFor="text">Text</label>
+            <textarea name="text" />
+          </div>
+
+          <div className="submit" style={{
+              display: "flex",
+              justifyContent: "end",
+            }}>
+            <Button text="SUMMIT" /></div>
+        </form>
+      </div>
+
+      <div className={styles.contact_Image}></div>
+    </section>
+  );
 };
 
 export default ContactForm;
