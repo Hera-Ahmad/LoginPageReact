46d3ca22e617743b2d7be35822153ce40218c46f
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-17 16:01:23 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import contactHeader from "./components/contactHeader/contactHeader:";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
 return (
     <div>
         <Navigation/>
+        <contactHeader/>
     </div>
 );
 };
